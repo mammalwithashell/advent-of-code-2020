d1854eba132f5703ee9365fbb6d91a1d8a16540e
james <james.peters97@outlook.com>
2021-01-05 21:55:22 -0600

    day5 pt 2

--- Day 5/main.py
+++ Day 5/main.py
@@ -16,6 +16,7 @@ with open("Day 5\input.txt", 'r') as input_file:
 
 seatids =[]
 output = []
+seats = [[True]*8 for _ in range(128)]
 for instruction in seat_things:
     row = 0
     count = 0
@@ -36,7 +37,7 @@ for instruction in seat_things:
             if letter == "R":
                 column = col_upper
             else:
-                column = col_lower
+                column = col_lower + 1
                 
         if letter in "FB":
             # finds number for row
@@ -55,10 +56,13 @@ for instruction in seat_things:
         
 
     # print(f"Instruction: {instruction}, Row: {row}, Column: {column}")
-    output.append(f"Instruction: {instruction}, Row: {row}, Column: {column}, Seat-ID: {(row -1)* 8 + column-1}")
+    output.append(f"Row: {row}, Column: {column}")
+    seats[int(row -1)][int(column-1)] = False
     seatids.append((row -1)* 8 + column-1)
     
-print(max(seatids))
-            
+pprint(seats)
+pprint(seatids)
             
 
+# the answer for part 1 is max(seatids)
+# the answer for part 2 is in the list of seats
